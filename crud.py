75c6d966277ab60aa2d13c56fcac18fd5b65a8ae
youralien <ryan.louie@students.olin.edu>
2015-11-29 14:43:28 -0500

    converted indents to 4 spaces

--- crud.py
+++ crud.py
@@ -3,41 +3,41 @@ import sqlite3
 
 class DBWrapper(object):
 
-	def __init__(self, db_name):
-		self.conn = sqlite3.connect(db_name)
+    def __init__(self, db_name):
+        self.conn = sqlite3.connect(db_name)
 
-	def create_comments_table(self):
-		c = self.conn.cursor()
-		c.execute(
-		    """CREATE TABLE comments 
-		    (
-		    link TEXT PRIMARY KEY,
-		    person TEXT,
-		    time TEXT,
-		    body TEXT,
-		    repo_owner TEXT,
-		    repo_name TEXT,
-		    pr_number INT,
-		    pr_updated_at TEXT
-		    )"""
-		)
-		self.conn.commit()
+    def create_comments_table(self):
+        c = self.conn.cursor()
+        c.execute(
+            """CREATE TABLE comments 
+            (
+            link TEXT PRIMARY KEY,
+            person TEXT,
+            time TEXT,
+            body TEXT,
+            repo_owner TEXT,
+            repo_name TEXT,
+            pr_number INT,
+            pr_updated_at TEXT
+            )"""
+        )
+        self.conn.commit()
 
-	def upsert_comment(self, link, person, time, body, repo_owner, repo_name, pr_number, pr_updated_at):
-		c = self.conn.cursor()
-		c.execute("""INSERT OR IGNORE INTO comments
-			(link, person, time, body, repo_owner, repo_name, pr_number, pr_updated_at)
-			VALUES (?, ?, ?, ?, ?, ?, ?, ?)""",
-			(link, person, time, body, repo_owner, repo_name, pr_number, pr_updated_at))
-		c.execute("""UPDATE comments SET
-			link=?,
-			person=?,
-			time=?,
-			body=?,
-			repo_owner=?,
-			repo_name=?,
-			pr_number=?,
-			pr_updated_at=?
-			WHERE link=? """,
-			(link, person, time, body, repo_owner, repo_name, pr_number, pr_updated_at, link))
-		self.conn.commit()
+    def upsert_comment(self, link, person, time, body, repo_owner, repo_name, pr_number, pr_updated_at):
+        c = self.conn.cursor()
+        c.execute("""INSERT OR IGNORE INTO comments
+            (link, person, time, body, repo_owner, repo_name, pr_number, pr_updated_at)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)""",
+            (link, person, time, body, repo_owner, repo_name, pr_number, pr_updated_at))
+        c.execute("""UPDATE comments SET
+            link=?,
+            person=?,
+            time=?,
+            body=?,
+            repo_owner=?,
+            repo_name=?,
+            pr_number=?,
+            pr_updated_at=?
+            WHERE link=? """,
+            (link, person, time, body, repo_owner, repo_name, pr_number, pr_updated_at, link))
+        self.conn.commit()
--- utils.py
+++ utils.py
@@ -1,23 +1,23 @@
 from __future__ import unicode_literals
 
 def format_quotes(string):
-	if isinstance(string, str):
-		string.replace('\"', r'\"')
-		string.replace("\'", r"\'")
-	return string
+    if isinstance(string, str):
+        string.replace('\"', r'\"')
+        string.replace("\'", r"\'")
+    return string
 
 if __name__ == '__main__':
-	
-	foo = u'@cooijmanstim Thanks for picking this up.\r\n\r\n> we need to think about how to save/load the BN parameters. \r\n\r\nBy \\"saved\\" you mean serialized with a checkpoint? It seems like where to save that entire test-time graph is more the issue, isn\'t it? One would like to be able to get access to and evaluate it.\r\n\r\nOne thing I think we should leave up to the user is whether to adapt these via some additional squared error objective function passed as a term of the cost function to GradientDescent (which seems like a straightfoward and easy way to do it) or just via some simple moving average updates added with `add_updates`. In either case I don\'t think it\'s a problem to consider these quantities PARAMETERs as they don\'t appear in the train-time graph and therefore won\'t be adapted if you do gradient descent on the train-time graph\'s (single) output.\r\n\r\nThere\'s a larger arc to this story which is whether the graph substitution framework is really appropriate for this situation. Other frameworks are tackling this by adding a \\"Brick\\" equivalent into the original model object hierarchy, which makes it a little easier to figure out what\'s going on. In that kind of a setup, from a  Blocks perspective, we\'d essentially have two apply methods, one for train time and one for test time. In that situation, however, we\'d need some way of conveniently excluding the learned means and variances from gradient descent on the real objective function, which probably means a new parent role for PARAMETER, like ADAPTABLE or something.'
+    
+    foo = u'@cooijmanstim Thanks for picking this up.\r\n\r\n> we need to think about how to save/load the BN parameters. \r\n\r\nBy \\"saved\\" you mean serialized with a checkpoint? It seems like where to save that entire test-time graph is more the issue, isn\'t it? One would like to be able to get access to and evaluate it.\r\n\r\nOne thing I think we should leave up to the user is whether to adapt these via some additional squared error objective function passed as a term of the cost function to GradientDescent (which seems like a straightfoward and easy way to do it) or just via some simple moving average updates added with `add_updates`. In either case I don\'t think it\'s a problem to consider these quantities PARAMETERs as they don\'t appear in the train-time graph and therefore won\'t be adapted if you do gradient descent on the train-time graph\'s (single) output.\r\n\r\nThere\'s a larger arc to this story which is whether the graph substitution framework is really appropriate for this situation. Other frameworks are tackling this by adding a \\"Brick\\" equivalent into the original model object hierarchy, which makes it a little easier to figure out what\'s going on. In that kind of a setup, from a  Blocks perspective, we\'d essentially have two apply methods, one for train time and one for test time. In that situation, however, we\'d need some way of conveniently excluding the learned means and variances from gradient descent on the real objective function, which probably means a new parent role for PARAMETER, like ADAPTABLE or something.'
 
-	try:
-		print """ fail: "{}" """.format(foo)
-	except Exception, e:
-		print e
+    try:
+        print """ fail: "{}" """.format(foo)
+    except Exception, e:
+        print e
 
-	foo = format_quotes(foo)	
+    foo = format_quotes(foo)    
 
-	try:
-		print """ succeed: "{}" """.format(foo)
-	except Exception, e:
-		print e
+    try:
+        print """ succeed: "{}" """.format(foo)
+    except Exception, e:
+        print e
