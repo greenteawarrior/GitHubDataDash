abde66b1713a24675d4988ebb9d2b4c4003f40b8
youralien <ryan.louie@students.olin.edu>
2015-11-29 14:39:43 -0500

    fix by using sql syntax variable insertion instead of python string format. also more cached api calls :+1:

--- crud.py
+++ crud.py
@@ -25,24 +25,19 @@ class DBWrapper(object):
 
 	def upsert_comment(self, link, person, time, body, repo_owner, repo_name, pr_number, pr_updated_at):
 		c = self.conn.cursor()
-		insert_query = """INSERT OR IGNORE INTO comments
+		c.execute("""INSERT OR IGNORE INTO comments
 			(link, person, time, body, repo_owner, repo_name, pr_number, pr_updated_at)
-			VALUES
-			("{link}","{person}","{time}","{body}","{repo_owner}","{repo_name}",{pr_number},"{pr_updated_at}")
-			""".format(link=link, person=person, time=time, body=body, repo_owner=repo_owner, repo_name=repo_name, pr_number=pr_number, pr_updated_at=pr_updated_at)
-		print insert_query
-		c.execute(insert_query)
-		update_query = """UPDATE comments SET
-			link="{link}",
-			person="{person}",
-			time="{time}",
-			body="{body}",
-			repo_owner="{repo_owner}",
-			repo_name="{repo_name}",
-			pr_number={pr_number},
-			pr_updated_at="{pr_updated_at}"
-			WHERE link="{link}";
-			""".format(link=link, person=person, time=time, body=body, repo_owner=repo_owner, repo_name=repo_name, pr_number=pr_number, pr_updated_at=pr_updated_at)
-		print update_query
-		c.execute(update_query)
+			VALUES (?, ?, ?, ?, ?, ?, ?, ?)""",
+			(link, person, time, body, repo_owner, repo_name, pr_number, pr_updated_at))
+		c.execute("""UPDATE comments SET
+			link=?,
+			person=?,
+			time=?,
+			body=?,
+			repo_owner=?,
+			repo_name=?,
+			pr_number=?,
+			pr_updated_at=?
+			WHERE link=? """,
+			(link, person, time, body, repo_owner, repo_name, pr_number, pr_updated_at, link))
 		self.conn.commit()
