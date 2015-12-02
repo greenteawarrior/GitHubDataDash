33293fad6041d16176db07ad2c7b98d00f2b6374
youralien <ryan.louie@students.olin.edu>
2015-12-01 19:50:17 -0500

    added pull_requests to json

--- app.py
+++ app.py
@@ -7,8 +7,16 @@ def construct_repo_url(repo_owner, repo_name):
 	return "https://www.github.com/{repo_owner}/{repo_name}"\
 		.format(repo_owner=repo_owner, repo_name=repo_name)
 
+def construct_pull_request_url_v1(repo_owner, repo_name, number):
+	return "https://www.github.com/{repo_owner}/{repo_name}/pull/{n}"\
+		.format(repo_owner=repo_owner, repo_name=repo_name, n=number)
+
+def construct_pull_request_url_v2(comment_url):
+	return comment_url.split('#')[0]
+
 def get_comments_wrapup_for_repo(repo_owner, repo_name):
 	comments_res = dbw.get_comments_for_repo(repo_owner, repo_name)
+	pull_requests = {}
 	for i, result in enumerate(comments_res):
 		tmp = dict(zip(schema, result))
 		if i == 0:
@@ -27,7 +35,13 @@ def get_comments_wrapup_for_repo(repo_owner, repo_name):
 			"time": tmp["time"]
 		}
 		wrapup["comments"].append(comment)
-		# add PR info
+		pull_requests[tmp["pr_number"]] = construct_pull_request_url_v2(tmp['link'])
+	# add the pull requests at the end
+	for number, url in pull_requests.iteritems():
+		wrapup["pull_requests"].append({
+			"number": number,
+			"url": url
+		})
 	return wrapup
 
 if __name__ == '__main__':
